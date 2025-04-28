1688d43f700d39c9b02b8a71b7502563c45e7d81
robdicoco <pavusa@gmail.com>
2025-04-27 21:18:01 -0300

    Refactor App component to implement a navigation system for multiple counter pages, remove unused HelloWorld contract code, and update CSS for improved layout and styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,129 +1,73 @@
-import React, { useRef } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-import { DefaultProvider, sha256, toHex, PubKey, bsv, TestWallet, Tx, toByteString } from "scrypt-ts";
-import { Helloworld } from "./contracts/helloworld";
+import PageSC03CouterDep from "./PageSC03CouterDep";
+import PageSC04CounterInc from "./PageSC04CounterInc"
+import PageSC05CounterDec from "./PageSC05CounterDec"
+import PageSC06CounterFinish from "./PageSC06CounterFinish"
 
-
-const provider = new DefaultProvider({network: bsv.Networks.testnet});
-let Alice: TestWallet
-let signerExt: TestWallet
-const privateKey = bsv.PrivateKey.fromHex("3c2ffdbb0a57c0cff0deacba15a92bf1d218dda9a9c7c668dd0640a6204b6394", bsv.Networks.testnet)   
 
 function App() {
 
-  const deploy = async (amount: any) => {
+  const [currentPage, setCurrentPage] = useState<string>('Counter');
 
-    Alice = new TestWallet(privateKey, provider)
+  const [showContDropdown, setShowContDropdown] = useState<boolean>(false);
+  const [showSCDropdown, setShowSCDropdown] = useState<boolean>(false);
 
-    try {
-      const signer = Alice
-      const message = toByteString('hello world', true)
-      const instance = new Helloworld(sha256(message))
-      
-      await instance.connect(signer);
-          
-      const deployTx = await instance.deploy(100)
-      //console.log('Helloworld contract deployed: ', deployTx.id)
-      //alert('deployed: ' + deployTx.id)
-
-      let tx = new bsv.Transaction(deployTx);
-
-      //tx.fromHex(deployTx)
-
-      console.log('Helloworld contract deployed: ', tx.id)
-      alert('deployed: ' + tx.id)
-
-
-    } catch (e) {
-      console.error('deploy HelloWorld failes', e)
-      alert('deploy HelloWorld failes')
-    }
+  const handlePageChange = (page: string) => {
+    setCurrentPage(page);
+    setShowContDropdown(false);
+    setShowSCDropdown(false);
   };
 
-
-  const interact = async (amount: any) => {
-
-    Alice = new TestWallet(privateKey, provider)
-
-    try {
-
-      const signer = Alice
-      const message = toByteString('hello world', true)
-      let tx = new bsv.Transaction
-      tx = await provider.getTransaction(txid.current.value)
-  
-      console.log('Current State TXID: ', tx.id)
-
-      const instance = Helloworld.fromTx(tx, 0) 
-      await instance.connect(signer)
-  
-      const { tx: callTx } = await instance.methods.unlock(message)
-      console.log('Helloworld contract `unlock` called: ', callTx.id)
-      alert('unlock: ' + callTx.id)
-  
-    } catch (e) {
-      console.error('deploy HelloWorld failes', e)
-      alert('deploy HelloWorld failes')
-    }
-  };
-
-  const txid = useRef<any>(null);
 
   return (
     <div className="App">
-        <header className="App-header">
+            <nav className="navbar">
 
-        <h2 style={{ fontSize: '34px', paddingBottom: '5px', paddingTop: '5px'}}>Hello World - sCrypt & React</h2>
+              <div className="dropdown">
+                <button className="button" 
+                    onClick={() => {setShowSCDropdown(!showSCDropdown);}}>
+                  Smart Contracts
+                </button>
+                {showSCDropdown && (
+                  <div className="dropdown-content">
 
-        <div style={{ textAlign: 'center' }}>
-                  
-                  <label style={{ fontSize: '14px', paddingBottom: '5px' }}
-                    >Press Deploy to Create the Contract:  
-                  </label>     
-        </div>
-        <button className="insert" onClick={deploy}
-                style={{ fontSize: '14px', paddingBottom: '2px', marginLeft: '5px'}}
-        >Deploy</button>
-                              
+                    <button className="dropdown-button" 
+                          onClick={() => {setShowContDropdown(!showContDropdown); }}>
+                        Counter
+                    </button>
+                    {showContDropdown && (
+                        <div className="button">
+                          <button className="dropdown-button-right" style={{ border: '1px solid #fff', marginLeft: 'auto',  marginRight: '0', 
+                          fontSize: '12px',color: 'white', background: '#323a3c', width: '50%'}} onClick={() => handlePageChange('Counter')}>
+                            Deploy
+                          </button>
+                          <button className="dropdown-button-right" style={{ border: '1px solid #fff', marginLeft: 'auto', marginRight: '0', 
+                          fontSize: '12px',color: 'white', background: '#323a3c', width: '50%'}} onClick={() => handlePageChange('Counter02')}>
+                            Increment
+                          </button>
+                          <button className="dropdown-button-right" style={{ border: '1px solid #fff', marginLeft: 'auto', marginRight: '0', 
+                          fontSize: '12px',color: 'white', background: '#323a3c', width: '50%'}} onClick={() => handlePageChange('Counter03')}>
+                            Decrement
+                          </button>
+                          <button className="dropdown-button-right" style={{ border: '1px solid #fff', marginLeft: 'auto', marginRight: '0', 
+                          fontSize: '12px',color: 'white', background: '#323a3c', width: '50%'}} onClick={() => handlePageChange('Counter04')}>
+                            Finish
+                          </button>
+                        </div>
+                    )}
 
-        {/* <img src={logo} className="App-logo" alt="logo" /> 
-        
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=MnfzAx-A1oA&list=PLe_C0QmVAyivD40DXYtUVSAFmx7ntGjJZ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn sCrypt
-        </a>
-        */}
+                  </div>
+                )}  
+              </div>
 
-        <div>
+            </nav>
+      {currentPage === 'Counter' && <PageSC03CouterDep />}
+      {currentPage === 'Counter02' && <PageSC04CounterInc />}
+      {currentPage === 'Counter03' && <PageSC05CounterDec />}
+      {currentPage === 'Counter04' && <PageSC06CounterFinish />}
 
-          <div style={{ textAlign: 'center' }}>
-                
-                <label style={{ fontSize: '14px', paddingBottom: '2px' }}
-                  >Inform the Current TXID and press Unlock to use the Contract:  
-                </label>     
-          </div>
-
-          <div style={{ display: 'inline-block', textAlign: 'center' }}>
-            <label style={{ fontSize: '14px', paddingBottom: '5px' }}  
-                > 
-                    <input ref={txid} type="hex" name="PVTKEY1" min="1" defaultValue={'TXID'} placeholder="hex" />
-                </label>     
-            </div>
-            <div style={{ display: 'inline-block', textAlign: 'center' }}>
-                
-                <button className="insert" onClick={interact}
-                    style={{ fontSize: '14px', paddingBottom: '2px', marginLeft: '20px'}}
-                >Unlock</button>
-
-            </div>
-        </div>                      
-      </header>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-import { Helloworld } from './contracts/helloworld';
-import artifact from '../artifacts/helloworld.json';
-Helloworld.loadArtifact(artifact);
+import { Counter } from './contracts/counter';
+
+import artifact02 from '../artifacts/counter.json';
+
+Counter.loadArtifact(artifact02)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,8 +16,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
